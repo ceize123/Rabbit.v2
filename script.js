71777573c09566795416f1040568c8a18a6baf5e
ceize123 <hahaeating1@gmail.com>
2020-11-07 12:23:00 +0800

    test2

--- script.js
+++ script.js
@@ -162,14 +162,19 @@ $('.circle, .mouseoverImg, .recBtn, .talk').mouseout(function(){
   refresh2();
 });
 
-
+// Ipad
 // hamburger
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".navbar");
 const links = document.querySelectorAll(".navbar ul li");
+const lines = document.querySelectorAll(".line");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
+  // foreach裡面的參數可以隨意取
+  lines.forEach(line => {
+    line.classList.toggle("purpleToggle");
+  });
   links.forEach(link => {
     link.classList.toggle("fade");
   });
@@ -177,7 +182,11 @@ hamburger.addEventListener("click", () => {
 
 $(document).ready(function(){
   $('.navBtn').click(function(){
-    $(this).next('nav ul .navToggle').slideToggle();
+    $(this).next('nav ul .navToggle').toggle();
     $(this).children('span').toggleClass('rotate');
+  });
+  $('.hamburger').click(function(){
+    $('nav ul .navToggle').slideUp();
+    $('.navBtn').children('span').removeClass('rotate');
   });
 });
