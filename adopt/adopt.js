4775102311f994416928a4f5135fdc6f4b2a87b4
ceize123 <hahaeating1@gmail.com>
2020-11-14 00:15:01 +0800

    finish ipad

--- adopt/adopt.js
+++ adopt/adopt.js
@@ -8,6 +8,7 @@ const lines = document.querySelectorAll(".line");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   // foreach裡面的參數可以隨意取
+  // 漢堡變色
   lines.forEach(line => {
     line.classList.toggle("purpleToggle");
   });
@@ -27,11 +28,14 @@ $(document).ready(function(){
   });
 });
 
+
 // findRab
+  let count = 0;
+
   $('.findRabBtn').click(function(){
     $('.findRabSectionIpad').css('margin-left',0)
   });
-  //
+
   // $('.choseOutside > .choseList').click(function(){
   //   $('.choseInside').css('left',0);
   // });
@@ -48,4 +52,46 @@ $(document).ready(function(){
   $('.findRabRight').click(function(){
     $('.findRabSectionIpad').css('margin-left','-100vw');
     $('.choseInside').css('left','-76.2vw');
+  });
+
+  // cleanSection
+  // $('.choseInside > .choseList').each(function(index){
+  //   $(this).click(function(){
+  //     $('.choosenSection').append('<div class="choosenItem"></div>');
+  //     $('.choosenItem:eq(0)').text(this);
+  //   });
+  // });
+
+
+  const checkValue = new Set();
+
+  $('.choseInside > .choseList').click(function() {
+    // 檢查set裡面有沒有這個value，沒有的話才新增
+    if ( checkValue.has($(this).attr('value')) == false ) {
+    // 在cleanAll外部新增，所以用before
+      $('.cleanAll').before('<div class="choosenItem">' + $(this).attr('value') + '</div>');
+      checkValue.add($(this).attr('value'));
+      $('.cleanAll p').css('opacity', 1);
+      count++;
+    };
+  });
+
+  // 個別清除 delegate
+  $('.choosenSection').delegate('.choosenItem','click',function(){
+    $(this).remove();
+    checkValue.delete($(this).text());
+    count--;
+    if (count <= 0){
+      $('.cleanAll p').css('opacity', 0);
+    };
+  });
+
+
+
+  // 清空
+  $('.cleanAll').click(function(){
+    $('.choosenItem').remove();
+    checkValue.clear();
+    $('.cleanAll p').css('opacity', 0);
+    count = 0;
   });
